6a7c4bcc55f4080fb132a8fd397711269cbe0dad
Unknown <vaibhavjain01@hotmail.com>
2017-11-24 18:09:04 -0500

    Un-necessary files deleted

---
+++ Workspace/DD_PROJECT/src/rmiinterface.java
@@ -0,0 +1,18 @@
+import java.rmi.*;
+import java.util.List;
+
+public interface rmiinterface extends Remote{
+	public String getMsg() throws RemoteException;
+	
+	/* Admin */
+	public Integer createRoom(String adminId, Integer roomNumber, String date, 
+			List<String> listOfTimeSlots) throws RemoteException;
+	public Integer deleteRoom(String adminId, Integer roomNumber, String date, 
+			List<String> listOfTimeSlots) throws RemoteException;
+	
+	/* Student */
+	public String bookRoom(String studentId, Integer roomNumber, String date, 
+			String timeSlot) throws RemoteException;
+	public String getAvailableTimeSlot(String studentId, String date) throws RemoteException;
+	public Integer cancelBooking(String studentId, String bookingID) throws RemoteException;
+}
